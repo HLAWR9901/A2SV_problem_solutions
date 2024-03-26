e2615f7ea331daa254bb477fcb7de1cbe52180ae
HLAWR9901 <147336275+HLAWR9901@users.noreply.github.com>
2024-03-26 13:25:10 +0300

    Create Toeplitz Matrix.py

---
+++ Toeplitz Matrix.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if matrix[i][j] != matrix[i - 1][j - 1]:
+                    return False
+        return True
+'''
+# Incase testing is needed
+solution = Solution()
+matrix1 = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+print(solution.isToeplitzMatrix(matrix1))
+matrix2 = [[1,2],[2,2]]
+print(solution.isToeplitzMatrix(matrix2))
+'''
